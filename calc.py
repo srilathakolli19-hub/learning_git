a9e9c5fda2dfb7eeb9f6a41a9568187bd7e6ec51
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-09-25 20:03:34 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,12 @@
+def add(x,y):
+  return x+y
+def subtract(x,y):
+  return x-y
+def divide(x,y):
+  return x/y
+def multiply(x,y):
+  return x*y
+print(add(4,5))
+print(subtract(4,5))
+print(divide(4,5))
+print(multiply(4,5))
